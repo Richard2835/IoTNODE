528610316b8899e9f2ef2c975064c44f14ee358b
Richard Labat <riso2835@azet.sk>
2021-06-29 18:46:34 +0200

    uloha 1 komplet

--- src/main/java/sk/fri/uniza/App.java
+++ src/main/java/sk/fri/uniza/App.java
@@ -1,10 +1,102 @@
 package sk.fri.uniza;
 
+import retrofit2.Call;
+import retrofit2.Response;
+import sk.fri.uniza.model.Location;
+import sk.fri.uniza.model.WeatherData;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello IoT!
  */
 public class App {
     public static void main(String[] args) {
+        IotNode iotNode = new IotNode();
+        // Vytvorenie požiadavky na získanie údajov o aktuálnom počasí z
+        // meteo stanice s ID: station_1
+        Call<Map<String, String>> currentWeather =
+                iotNode.getWeatherStationService()
+                        .getCurrentWeatherAsMap("station_1",
+                                List.of("time", "date",
+                                        "airTemperature"));
+        try {
+            // Odoslanie požiadavky na server pomocou REST rozhranie
+            Response<Map<String, String>> response = currentWeather.execute();
 
+            if (response.isSuccessful()) { // Dotaz na server bol úspešný
+                //Získanie údajov vo forme Mapy stringov
+                Map<String, String> body = response.body();
+                System.out.println(body);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Vytvorenie požiadavky na získanie údajov o všetkých meteo staniciach
+        Call<List<Location>> stationLocations =
+                iotNode.getWeatherStationService().getStationLocations();
+
+        try {
+            Response<List<Location>> response = stationLocations.execute();
+
+            if (response.isSuccessful()) { // Dotaz na server bol úspešný
+                //Získanie údajov vo forme Zoznam lokacií
+                List<Location> body = response.body();
+
+                System.out.println(body);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Vytvorenie požiadavky na získanie údajov o aktuálnom počasí z
+        // meteo stanice s ID: station_1
+        Call<WeatherData> currentWeatherPojo =
+                iotNode.getWeatherStationService()
+                        .getCurrentWeather("station_1");
+
+
+        try {
+            // Odoslanie požiadavky na server pomocou REST rozhranie
+            Response<WeatherData> response = currentWeatherPojo.execute();
+
+            if (response.isSuccessful()) { // Dotaz na server bol úspešný
+                //Získanie údajov vo forme inštancie triedy WeatherData
+                WeatherData body = response.body();
+                System.out.println(body);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+         Call<List<WeatherData>> historyWeather =
+                iotNode.getWeatherStationService().getHistoryWeather("station_1","29/06/2021 11:00","29/06/2021 16:00");
+        try {
+            Response<List<WeatherData>> response = historyWeather.execute();
+            if (response.isSuccessful()) { // Dotaz na server bol úspešný
+                //Získanie údajov vo forme Zoznam lokacií
+                List<WeatherData> body = response.body();
+                System.out.println(body);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Call<List<WeatherData>> historyWeather2 =
+                iotNode.getWeatherStationService().getHistoryWeather("station_1","29/06/2021 10:00","29/06/2021 11:00", List.of("date","time","airTemperature"));
+        try {
+            Response<List<WeatherData>> response = historyWeather2.execute();
+            if (response.isSuccessful()) { // Dotaz na server bol úspešný
+                //Získanie údajov vo forme Zoznam lokacií
+                List<WeatherData> body = response.body();
+                System.out.println(body);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/sk/fri/uniza/IotNode.java
+++ src/main/java/sk/fri/uniza/IotNode.java
@@ -1,9 +1,55 @@
 package sk.fri.uniza;
 
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import sk.fri.uniza.api.WeatherStationService;
+import sk.fri.uniza.model.WeatherData;
+
+import java.io.IOException;
+import java.util.List;
 
 public class IotNode {
+    private final Retrofit retrofit;
+    private final WeatherStationService weatherStationService;
 
+    public IotNode() {
+
+        retrofit = new Retrofit.Builder()
+                // Url adresa kde je umietnená WeatherStation služba
+                .baseUrl("http://localhost:9000/")
+                // Na konvertovanie JSON objektu na java POJO použijeme
+                // Jackson knižnicu
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+        // Vytvorenie inštancie komunikačného rozhrania
+        weatherStationService = retrofit.create(WeatherStationService.class);
+
+    }
+
+    public WeatherStationService getWeatherStationService() {
+        return weatherStationService;
+    }
+
+    double getAverageTemperature(String station,String from, String to)
+    {
+        Call<List<WeatherData>> historyWeather2 =
+                getWeatherStationService().getHistoryWeather(station, from, to, List.of("airTemperature"));
+
+        try {
+            // Odoslanie požiadavky na server pomocou REST rozhrania
+            Response<List<WeatherData>> response = historyWeather2.execute();
+
+            if (response.isSuccessful()) { // Dotaz na server bol úspešný
+                List<WeatherData> body = response.body();
+                System.out.println(body);
+                return body.stream().mapToDouble(WeatherData::getAirTemperature).sum() / body.size();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
